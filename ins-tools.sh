46cf5e91bc4417f87e0ff281d16e824f7379bd38
Oprahouse9 <116130475+Oprahouse9@users.noreply.github.com>
2023-01-26 19:47:37 -0500

    Add files via upload

---
+++ ins-tools.sh
@@ -0,0 +1,57 @@
+sudo apt update
+sudo apt install python3
+sudo apt install pip
+git clone https://github.com/nitefood/asn.git
+cd asn
+sudo apt install jq mtr whois ipcalc grepcidr ncat aha
+cd /home/kali
+ls -l
+git clone https://github.com/7zx/overload
+cd overload/
+pip install -r requirements.txt
+cd /home/kali
+ls -l
+git clone https://github.com/TheRealDalunacrobate/DaProfiler.git
+cd DaProfiler
+pip install -r requirements.txt
+cd /home/kali
+ls -l
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+cd Cam-Hackers
+pip install -r requirements.txt
+cd /home/kali
+ls -l
+git clone https://github.com/beefproject/beef.git
+cd beef
+nano config.yaml
+./install
+cd /home/kali
+ls -l
+git clone https://github.com/maari-krish/Vpn-Detector.git
+ls -l
+git clone https://github.com/p1ngul1n0/blackbird
+cd blackbird
+pip install -r requirements.txt
+cd /home/kali
+ls -l
+git clone https://github.com/maari-krish/Email-scan.git
+ls -l
+git clone https://github.com/CodeX-ID/Temp-mail
+ls -l
+git clone https://github.com/hackerxphantom/xransom
+cd xransom
+bash install.sh
+cd /home/kali
+ls -l
+git clone https://github.com/Oprahouse9/botnet.git
+cd botnet
+pip install -r requirements.txt
+cd /home/kali
+ls -l
+git clone https://github.com/Oprahouse9/car-osint.git
+git clone https://github.com/TechnoFuge/PhoneInfoga.git
+git clone https://github.com/Oprahouse9/msf-apk.git
+sudo apt install spiderfoot theharvester metasploit-framework
+sudo apt install wafw00f knockpy dnsrecon dnsenum sherlock exploitdb
+pip install autopwn-suite h8mail
+sudo wget -qO- https://github.com/Bhaviktutorials/shark/raw/master/setup | sudo bash
