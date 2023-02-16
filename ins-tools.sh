4da935b4601bf85cc06f0f4eb273580b42d6dbb3
Oprahouse9 <116130475+Oprahouse9@users.noreply.github.com>
2023-02-16 00:47:26 -0500

    Update ins-tools.sh

--- ins-tools.sh
+++ ins-tools.sh
@@ -48,6 +48,11 @@ cd NmapScan
 pip install -r requirements.txt
 cd /home/kali
 ls -l
+git clone https://github.com/Oprahouse9/Profiler.git
+cd Profiler
+pip install -r requirements.txt
+cd /home/kali
+ls -l
 git clone https://github.com/Oprahouse9/car-osint.git
 git clone https://github.com/TechnoFuge/PhoneInfoga.git
 git clone https://github.com/Oprahouse9/msf-apk.git
